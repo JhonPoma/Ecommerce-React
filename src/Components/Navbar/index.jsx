55e7ab3d1c41045f0ce46e01ef94ae14c1f2ea39
JhonPoma <jpm11797@gmail.com>
2025-01-07 18:17:21 -0500

    estilizamos nuestro navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,43 +2,53 @@ import { NavLink } from "react-router-dom"
 
 const Navbar= ()=>{
 
+    //Este estilo se mostrara cuando este seleccionado la ruta
+    // EJEMPLO SIN USAR TAILWINDS, usamos un objeto JavaScript para manejar estilos.
+    let estiloActivado2 = {
+      textDecoration:"underline",
+      color:"green"
+    }
+    // USANDO TAILWINDS, usamos las clases de tailwinf para aplicar estilos.
+    const estiloActivado = 'underline underline-offset-4 text-green-500'
+
+
     return(
 
-        <nav>
+        <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
             {/* LADO IZQUIERDO */}
-            <ul>
-                <li>
-                  <NavLink to='/'>
+            <ul className="flex items-center gap-3">
+                <li className="font-semibold text-lg">
+                  <NavLink to='/' >
                     tienda
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/all'>
+                  <NavLink to='/all' style={({isActive})=>isActive?estiloActivado2:undefined}>
                     Todo
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/clothes'>
+                  <NavLink to='/clothes' style={({isActive})=>isActive?estiloActivado2:undefined}>
                     Clothes
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/electronics'>
+                  <NavLink to='/electronics' className={({isActive})=>isActive?estiloActivado:undefined} >
                     Electronics
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/furnitures'>
+                  <NavLink to='/furnitures' className={({isActive})=>isActive?estiloActivado:undefined}>
                     Furnitures
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/toys'>
+                  <NavLink to='/toys' className={({isActive})=>isActive?estiloActivado:undefined}>
                     Toys
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/others'>
+                  <NavLink to='/others' className={({isActive})=>isActive?estiloActivado:undefined}>
                     Others
                   </NavLink>
                 </li>
@@ -47,22 +57,22 @@ const Navbar= ()=>{
             </ul>
 
             {/* LADO DERECHO */}
-            <ul>
-                <li>
+            <ul className="flex items-center gap-3">
+                <li className="text-black/60">
                   jhon@gmail.com
                 </li>
                 <li>
-                  <NavLink to='/my-orders'>
+                  <NavLink to='/my-orders' className={({isActive})=>isActive?estiloActivado:undefined}>
                     MyOrders
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/my-account'>
+                  <NavLink to='/my-account' className={({isActive})=>isActive?estiloActivado:undefined}>
                     MyAccount
                   </NavLink>
                 </li>
                 <li>
-                  <NavLink to='/sign-in'>
+                  <NavLink to='/sign-in' className={({isActive})=>isActive?estiloActivado:undefined}>
                     SignIn
                   </NavLink>
                 </li>
